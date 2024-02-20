4e79b8a160cf1e09ccfc1f7efadd3303b92f13a9
Aditya Chaudhary <96983394+SNKYNinja@users.noreply.github.com>
2024-02-21 02:41:29 +0530

    feat: Complete game

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,110 @@
 "use client";
 
 import {
-	AlertDialog,
-	AlertDialogCancel,
-	AlertDialogContent,
-	AlertDialogTrigger,
-	AlertDialogHeader,
+    AlertDialog,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogTrigger,
+    AlertDialogHeader,
 } from "@/components/ui/alert-dialog";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { songData } from "./songData";
 
+function AudioPlayer({ src }: { src: string }) {
+    return (
+        <audio
+            src={src}
+            controls
+            preload="auto"
+            className="border-b-4 border-slate-400 rounded-full"
+        ></audio>
+    );
+}
+
 function App() {
-	const [data, setData] = useState(songData);
-	const [showAnswer, setAnswer] = useState(false);
+    const [data, setData] = useState(songData);
+    const [showAnswer, setAnswer] = useState(false);
 
-	const handleClick = (id: number) => {
-		const newData = [...data];
-		newData.map((i) => {
-			if (i.id == id) {
-				i.disabled = true;
-			}
-			return i;
-		});
-		setAnswer(false);
-		setData(newData);
-	};
+    const handleClick = (id: number) => {
+        const newData = [...data];
+        newData.map((i) => {
+            if (i.id == id) {
+                i.disabled = true;
+            }
+            return i;
+        });
+        setAnswer(false);
+        setData(newData);
+    };
 
-	return (
-		<div className="grid grid-rows-10 grid-cols-10 gap-4 m-4 text-center bold font-semibold text-slate-800 text-lg">
-			{data.map((item) => (
-				<AlertDialog>
-					<AlertDialogTrigger asChild>
-						<button className="btn" disabled={item.disabled}>
-							{item.id}
-						</button>
-					</AlertDialogTrigger>
-					<AlertDialogContent className="flex justify-center items-center flex-col">
-						{showAnswer && (
-							<AlertDialogHeader className="font-bold text-center text-xl">
-								Song: {item.name}
-								<br></br>Movie: {item.movie}
-							</AlertDialogHeader>
-						)}
-						<div className="flex justify-center items-center gap-3">
-							<audio src={`/${item.id}.mp3`} controls></audio>
-							<AlertDialogCancel
-								className="rounded-md bg-sky-500 hover:bg-sky-600 px-4 h-10 mt-4"
-								onClick={() => handleClick(item.id)}
-							>
-								Done
-							</AlertDialogCancel>
-							<button
-								className="rounded-md bg-green-500 hover:bg-green-600 px-4 h-10"
-								onClick={() => setAnswer(!showAnswer)}
-							>
-								Answer
-							</button>
-						</div>
-					</AlertDialogContent>
-				</AlertDialog>
-			))}
-		</div>
-	);
+    useEffect(() => {
+        document.addEventListener("keydown", (e) => {
+            if (e.key === "Escape") {
+                setAnswer(false);
+            }
+        });
+    }, []);
+
+    return (
+        <>
+            <h1 className="font-protest text-6xl mt-3 mb-7 text-center">
+                <span className="text-indigo-600">&#9834;</span> Uckersterra 2.0{" "}
+                <span className="text-indigo-500">~</span> Round 2{" "}
+                <span className="text-indigo-600">&#9834;</span>
+            </h1>
+            <div className="grid grid-rows-10 grid-cols-10 gap-3 m-4 text-center bold font-semibold text-slate-800 text-lg">
+                {data.map((item) => (
+                    <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                            <button
+                                className="bg-blue-500 border-blue-600 hover:bg-blue-500 hover:text-white text-white cursor-pointer h-16 transition-all px-6 py-2 rounded-lg border-b-[6px] outline-none hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px] disabled:bg-red-500 disabled:border-red-600 disabled:cursor-not-allowed"
+                                disabled={item.disabled}
+                            >
+                                {item.id}
+                            </button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent className="w-[40%] max-w-screen flex justify-center items-center flex-col h-auto border-b-4 border-slate-400">
+                            {showAnswer && (
+                                <AlertDialogHeader className="font-fredoka text-center text-xl capitalize ">
+                                    <div className="text-3xl">
+                                        <span className="font-semibold">
+                                            Song:
+                                        </span>{" "}
+                                        {item.name}
+                                    </div>
+                                    <div className="text-3xl">
+                                        <span className="font-semibold">
+                                            Movie:
+                                        </span>{" "}
+                                        {item.movie}
+                                    </div>
+                                </AlertDialogHeader>
+                            )}
+                            <div className="flex justify-center items-center gap-3">
+                                <AudioPlayer
+                                    src={`/${item.id}.mp3`}
+                                ></AudioPlayer>
+                                <AlertDialogCancel
+                                    className="cursor-pointer h-12 transition-all bg-blue-500 text-white px-6 py-2 rounded-lg border-blue-600 outline-none border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:bg-blue-500 hover:text-white hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px] disabled:bg-red-500 disabled:border-red-600 disabled:cursor-not-allowed"
+                                    onClick={() =>
+                                        handleClick(item.id as number)
+                                    }
+                                >
+                                    Done
+                                </AlertDialogCancel>
+                                <button
+                                    className="cursor-pointer h-12 transition-all bg-green-500 text-white px-6 py-2 rounded-lg border-green-600 outline-none border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:bg-green-500 hover:text-white hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px] disabled:bg-red-500 disabled:border-red-600 disabled:cursor-not-allowed"
+                                    onClick={() => setAnswer(!showAnswer)}
+                                >
+                                    Answer
+                                </button>
+                            </div>
+                        </AlertDialogContent>
+                    </AlertDialog>
+                ))}
+            </div>
+        </>
+    );
 }
 
 export default App;
--- src/songData.tsx
+++ src/songData.tsx
@@ -1,80 +1,518 @@
 interface SongData {
-	id: number;
-	name: string;
-	movie: string;
-	path: string;
-	disabled: boolean;
+    id: number | string;
+    name: string;
+    movie: string;
+    disabled?: boolean;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    audio?: any;
 }
 
 export const songData: SongData[] = [
-	{
-		id: 1,
-		name: "Ore Piya",
-		movie: "Aja Nachle",
-		path: `/1.mp3`,
-		disabled: false,
-	},
-	{
-		id: 2,
-		name: "Ladki Badi Anjani Hai",
-		movie: "Kuch Kuch Hota Hai",
-		path: "/2.mp3",
-		disabled: false,
-	},
-	{
-		id: 3,
-		name: "Bholi Si Surat",
-		movie: "Dil To Pagal Hai",
-		path: "/3.mp3",
-		disabled: false,
-	},
-	{
-		id: 4,
-		name: "Dil Kyu Yeh Mera",
-		movie: "Kites",
-		path: "/4.mp3",
-		disabled: false,
-	},
-	{
-		id: 5,
-		name: "Janam Janam",
-		movie: "Dilwale",
-		path: "/5.mp3",
-		disabled: false,
-	},
-	{
-		id: 6,
-		name: "Moh Moh Ke Dhage",
-		movie: "Dum Laga Ke Haisha",
-		path: "/6.mp3",
-		disabled: false,
-	},
-	{
-		id: 7,
-		name: "Senorita",
-		movie: "Zindagi Na Milegi Dobara",
-		path: "/7.mp3",
-		disabled: false,
-	},
-	{
-		id: 8,
-		name: "Long Drive Pe Chal",
-		movie: "Khiladi 786",
-		path: "/8.mp3",
-		disabled: false,
-	},
-	{
-		id: 9,
-		name: "Tum Hi Ho",
-		movie: "Aashiqui 2",
-		path: "/9.mp3",
-		disabled: false,
-	},
-	{
-		id: 10,
-		name: "Afghan Jalebi",
-		movie: "Phantom",
-		path: "/10.mp3",
-		disabled: false,
-	},
+    {
+        id: "1",
+        name: "SMJHAVAAN",
+        movie: "HUMPTY SHARMA KI DULHANIYA",
+    },
+    {
+        id: "2",
+        name: "APNA BANA LE",
+        movie: "BHEDIYA",
+    },
+    {
+        id: "3",
+        name: "HAWAYEIN",
+        movie: "JAB HERRY MET SEJAL",
+    },
+    {
+        id: "4",
+        name: "BULEYA ",
+        movie: "AE DIL HAI MUSHKIL",
+    },
+    {
+        id: "5",
+        name: "AAOGE JAB TUM ",
+        movie: "JAB WE MET",
+    },
+    {
+        id: "6",
+        name: "JASHAN E BAHARA ",
+        movie: "JODHA AKBAR",
+    },
+    {
+        id: "7",
+        name: "SHAYAD",
+        movie: "LOVE AAJ KAL 2",
+    },
+    {
+        id: "8",
+        name: "THE HUMMA SONG ",
+        movie: "OK JAANU",
+    },
+    {
+        id: "9",
+        name: "SAIYAARA ",
+        movie: "EK THA TIGER ",
+    },
+    {
+        id: "10",
+        name: "HUMSAFAR",
+        movie: "BADRINATH KI DULHANIYA",
+    },
+    {
+        id: "11",
+        name: "DILBAR ",
+        movie: "SATYAMEVJAYATE",
+    },
+    {
+        id: "12",
+        name: "DIL CHORI",
+        movie: "SONU KE TITU KI SWEETY",
+    },
+    {
+        id: "13",
+        name: "TUM HEE HO BANDHU",
+        movie: "COCKTAIL",
+    },
+    {
+        id: "14",
+        name: "KASHMIR MAI TU KANYA KUMARI ",
+        movie: "CHENNAI EXPRESS",
+    },
+    {
+        id: "15",
+        name: "DILDARA ",
+        movie: "RA.ONE",
+    },
+    {
+        id: "16",
+        name: "NASHE SE CHAD GAYI",
+        movie: "BEFIKRE",
+    },
+    {
+        id: "17",
+        name: "TWIST",
+        movie: "LOVE AAJ KAL",
+    },
+    {
+        id: "18",
+        name: "MANN MA EMOTION JAAGE ",
+        movie: "DILWALE",
+    },
+    {
+        id: "19",
+        name: "MAI TERA BOYFRIEND",
+        movie: "RAABTA",
+    },
+    {
+        id: "20",
+        name: "JAANE KYU",
+        movie: "DOSTANA ",
+    },
+    {
+        id: "21",
+        name: "BESHARMI KI HEIGHT ",
+        movie: "MAI TERA HERO",
+    },
+    {
+        id: "22",
+        name: "MASAKALLI",
+        movie: "DELHI 6",
+    },
+    {
+        id: "23",
+        name: "BOL NA HALKE ",
+        movie: "JHOOM BARABAR JHOOM",
+    },
+    {
+        id: "24",
+        name: "PREM KI NAIYA",
+        movie: "AJAB PREM KI GAJAB KAHANI",
+    },
+    {
+        id: "25",
+        name: "NADAAN PARINDE ",
+        movie: "ROCKSTAR",
+    },
+    {
+        id: "26",
+        name: "CHASHNI ",
+        movie: "BHARAT",
+    },
+    {
+        id: "27",
+        name: "LAUNG DA LASKARA ",
+        movie: "PATIALA HOUSE",
+    },
+    {
+        id: "28",
+        name: "TERE BIN ",
+        movie: "SIMMBA",
+    },
+    {
+        id: "29",
+        name: "JEENE LAGA HU",
+        movie: "RAMAIYA VASTAMAIYA",
+    },
+    {
+        id: "30",
+        name: "ISHQ WALA LOVE ",
+        movie: "STUDENT OF THE YEAR",
+    },
+    {
+        id: "31",
+        name: "O MAAHI O MAAHI ",
+        movie: "DUNKI",
+    },
+    {
+        id: "32",
+        name: "JAB SE TERE NAINA ",
+        movie: "SAWARIYA",
+    },
+    {
+        id: "33",
+        name: "NADIYO PAAR ",
+        movie: "ROOHI",
+    },
+    {
+        id: "34",
+        name: "WHAT JHOOMKA",
+        movie: "ROCKY AUR RANI KI PREM KAHANI",
+    },
+    {
+        id: "35",
+        name: "JEHDA NSAHA",
+        movie: "AN ACTION HERO",
+    },
+    {
+        id: "36",
+        name: "CHIKNI CHAMELI",
+        movie: "AGNEEPATH",
+    },
+    {
+        id: "37",
+        name: "PSYCHO SAIYAAN",
+        movie: "SAHOO",
+    },
+    {
+        id: "38",
+        name: "COCA COLA",
+        movie: "LUKA CHUPPI",
+    },
+    {
+        id: "39",
+        name: "CHOGADA",
+        movie: "LOVE YATRI",
+    },
+    {
+        id: "40",
+        name: "HOOKAAH BAAR ",
+        movie: "KHILADI 786",
+    },
+    {
+        id: "41",
+        name: "TUMHE JO MAINE DEKHA",
+        movie: "MAIN HOON NA",
+    },
+    {
+        id: "42",
+        name: "CHITIYA KALAIYAN",
+        movie: "ROY",
+    },
+    {
+        id: "43",
+        name: "ISHQ BULAVA ",
+        movie: "HASEE TOH PHASEE",
+    },
+    {
+        id: "44",
+        name: "AGAR TUM SAATH HO",
+        movie: "TAMASHA",
+    },
+    {
+        id: "45",
+        name: "LAT LAG GAYI",
+        movie: "RACE 2",
+    },
+    {
+        id: "46",
+        name: " SAJDA",
+        movie: "MY NAME IS KHAN ",
+    },
+    {
+        id: "47",
+        name: "NAJAA",
+        movie: "SOORYAVANSHI",
+    },
+    {
+        id: "48",
+        name: "YAAR NA MILE",
+        movie: "KICK",
+    },
+    {
+        id: "49",
+        name: "TUM TAK",
+        movie: "RANJHANA",
+    },
+    {
+        id: "50",
+        name: "O BEDARDEYA",
+        movie: "TU JHUTHI MAI MAKAAR",
+    },
+    {
+        id: "51",
+        name: "CHAND SIFARISH",
+        movie: "FANAA",
+    },
+    {
+        id: "52",
+        name: "JEENA JEENA ",
+        movie: "BADLAPUR",
+    },
+    {
+        id: "53",
+        name: "KHUDA JAANE ",
+        movie: "BACHNA E HASEENO",
+    },
+    {
+        id: "54",
+        name: "TERI ORE",
+        movie: "SINGH IS KING",
+    },
+    {
+        id: "55",
+        name: "TE AMO",
+        movie: "DUM MARO DUM",
+    },
+    {
+        id: "56",
+        name: "MOH MOH KE DHAAGE",
+        movie: "DUM LAGA KE HAISA",
+    },
+    {
+        id: "57",
+        name: "O RANGREZ",
+        movie: "BHAAG MILKA BHAAG",
+    },
+    {
+        id: "58",
+        name: "SAWARE",
+        movie: "PHANTOM",
+    },
+    {
+        id: "59",
+        name: "LADKI BADI ANJANI HAI",
+        movie: "KUCH KUCH TOH HOTA HAI",
+    },
+    {
+        id: "60",
+        name: "BHOLI SI SURAT",
+        movie: "DIL TOH PAGAL HAI",
+    },
+    {
+        id: "61",
+        name: "DIL KYU YEH MERA",
+        movie: "KITES",
+    },
+    {
+        id: "62",
+        name: "KABHI KABHI ADITI",
+        movie: "JAANE TU YA JAANE NA",
+    },
+    {
+        id: "63",
+        name: "MAIN AGAR KAHOON",
+        movie: "OM SHANTI OM",
+    },
+    {
+        id: "64",
+        name: "ISHQ SUFIYANA",
+        movie: "THE DIRTY PICTURE",
+    },
+    {
+        id: "65",
+        name: "MUQABLA",
+        movie: "STREET DANCER",
+    },
+    {
+        id: "66",
+        name: "HAR FUN MAULA",
+        movie: "KOI JAANE NA",
+    },
+    {
+        id: "67",
+        name: "O JAANE JAANA",
+        movie: "PYAAR KIYA TOH DARNA KYA",
+    },
+    {
+        id: "68",
+        name: "SAWAAR LOON",
+        movie: "LOOTERA",
+    },
+    {
+        id: "69",
+        name: "BHAAG DK BOSE",
+        movie: "DELHI BELLY",
+    },
+    {
+        id: "70",
+        name: "NAZAR NA LAG JAAYE ",
+        movie: "STREE ",
+    },
+    {
+        id: "71",
+        name: "JAG GHOOMEYA ",
+        movie: "SULTAN",
+    },
+    {
+        id: "72",
+        name: "DEKHTE DEKHTE",
+        movie: "BATTI GUL METER CHALU",
+    },
+    {
+        id: "73",
+        name: "ZAALIMA",
+        movie: "RAEES",
+    },
+    {
+        id: "74",
+        name: "TERA FITOOR",
+        movie: "GENIUS",
+    },
+    {
+        id: "75",
+        name: "TERA CHEHRA",
+        movie: "SANAM TERI KASAM",
+    },
+    {
+        id: "76",
+        name: "PANI DA RANG",
+        movie: "VICKY DONOR",
+    },
+    {
+        id: "77",
+        name: "AINVAYI AINVAYI",
+        movie: "BAND BAJA BAARAT",
+    },
+    {
+        id: "78",
+        name: "FIRST CLASS",
+        movie: "KALANK",
+    },
+    {
+        id: "79",
+        name: "JHAK MAAR KE",
+        movie: "DESI BOYZ",
+    },
+    {
+        id: "80",
+        name: "DEEWANA HAI DEKHO",
+        movie: "KABHI KHUSHI KABHI GUM",
+    },
+    {
+        id: "81",
+        name: "SURILI ANKHIYO WALE",
+        movie: "VEER",
+    },
+    {
+        id: "82",
+        name: "MATARGHASTI",
+        movie: "TAMASHA",
+    },
+    {
+        id: "83",
+        name: "MERA NAAM TU",
+        movie: "ZERO",
+    },
+    {
+        id: "84",
+        name: "BAAZIGAR O BAAZIGAR",
+        movie: "BAAZIGAR",
+    },
+    {
+        id: "85",
+        name: "SAAT SAMUNDAR",
+        movie: "VISWATMA",
+    },
+    {
+        id: "86",
+        name: "TERE MAST MAST DO NAIN",
+        movie: "DABANGG",
+    },
+    {
+        id: "87",
+        name: "MAIN RANG SHARBATON",
+        movie: "PHATA POSTER NIKLA HERO",
+    },
+    {
+        id: "88",
+        name: "PEHLI NAZAR MAIN",
+        movie: "RACE ",
+    },
+    {
+        id: "89",
+        name: "SADI GALI",
+        movie: "TANU WEDS MANU",
+    },
+    {
+        id: "90",
+        name: "OH HO HO ",
+        movie: "HINDI MEDIUM",
+    },
+    {
+        id: "91",
+        name: "DIL MAI BAJI GUITAR",
+        movie: "APNA SAPNA MONEY MONEY",
+    },
+    {
+        id: "92",
+        name: "ZARA ZARA",
+        movie: "REHNA HAI TERE DIL MAI",
+    },
+    {
+        id: "93",
+        name: "DIL NA JAANEYA",
+        movie: "GOOD NEWS",
+    },
+    {
+        id: "94",
+        name: "BOL DO NA ZARA",
+        movie: "AZHAR",
+    },
+    {
+        id: "95",
+        name: "EK DIL EK JAAN ",
+        movie: "PADMAVAT",
+    },
+    {
+        id: "96",
+        name: "PIYA O RE PIYA",
+        movie: "TERE NAAL LOVE HO GAYA",
+    },
+    {
+        id: "97",
+        name: "MANN MERA ",
+        movie: "TABLE NO.21",
+    },
+    {
+        id: "98",
+        name: "CHAMMAK CHALLO",
+        movie: "RA.ONE",
+    },
+    {
+        id: "99",
+        name: "YEH FITOOR MERA ",
+        movie: "FITOOR",
+    },
+    {
+        id: "100",
+        name: "SHIRT DA BUTTON",
+        movie: "KYA SUPER COOL HAI HUM",
+    },
 ];
+
+songData.forEach((song) => {
+    song.name = song.name.toLocaleLowerCase();
+    song.movie = song.movie.toLocaleLowerCase();
+    song.id = Number(song.id);
+    song.disabled = false;
+});
