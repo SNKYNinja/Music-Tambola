008152950aea0314ad2506267f646652afd0f0c3
Aditya Chaudhary <96983394+SNKYNinja@users.noreply.github.com>
2024-09-16 00:36:32 +0530

    Update songData.tsx

--- src/songData.tsx
+++ src/songData.tsx
@@ -9,506 +9,507 @@ interface SongData {
 
 export const songData: SongData[] = [
     {
-        id: "1",
-        name: "SMJHAVAAN",
-        movie: "HUMPTY SHARMA KI DULHANIYA",
+        "id": "1",
+        "name": "TUJHE AKSA BEACH GHUMA DOON",
+        "movie": "GOD TUSSI GREAT HO"
     },
     {
-        id: "2",
-        name: "APNA BANA LE",
-        movie: "BHEDIYA",
+        "id": "2",
+        "name": "O MERI LAILA",
+        "movie": "LAILA MAJNU"
     },
     {
-        id: "3",
-        name: "HAWAYEIN",
-        movie: "JAB HERRY MET SEJAL",
+        "id": "3",
+        "name": "SAJNI",
+        "movie": "LAPATA LADY"
     },
     {
-        id: "4",
-        name: "BULEYA ",
-        movie: "AE DIL HAI MUSHKIL",
+        "id": "4",
+        "name": "AKHIYAN GULAAB",
+        "movie": "TERI BAATON MAI ESA ULJHA JIYA"
     },
     {
-        id: "5",
-        name: "AAOGE JAB TUM ",
-        movie: "JAB WE MET",
+        "id": "5",
+        "name": "AAOGE JAB TUM",
+        "movie": "JAB WE MET"
     },
     {
-        id: "6",
-        name: "JASHAN E BAHARA ",
-        movie: "JODHA AKBAR",
+        "id": "6",
+        "name": "JASHAN E BAHARA",
+        "movie": "JODHA AKBAR"
     },
     {
-        id: "7",
-        name: "SHAYAD",
-        movie: "LOVE AAJ KAL 2",
+        "id": "7",
+        "name": "RANGI SARI",
+        "movie": "JUG JUG JEEYO"
     },
     {
-        id: "8",
-        name: "THE HUMMA SONG ",
-        movie: "OK JAANU",
+        "id": "8",
+        "name": "THE HUMMA SONG",
+        "movie": "OK JAANU"
     },
     {
-        id: "9",
-        name: "SAIYAARA ",
-        movie: "EK THA TIGER ",
+        "id": "9",
+        "name": "SAIYAARA",
+        "movie": "EK THA TIGER"
     },
     {
-        id: "10",
-        name: "HUMSAFAR",
-        movie: "BADRINATH KI DULHANIYA",
+        "id": "10",
+        "name": "TAREEAN",
+        "movie": "VEERE DI WEDDING"
     },
     {
-        id: "11",
-        name: "DILBAR ",
-        movie: "SATYAMEVJAYATE",
+        "id": "11",
+        "name": "HUMDARD",
+        "movie": "EK VILLAIN"
     },
     {
-        id: "12",
-        name: "DIL CHORI",
-        movie: "SONU KE TITU KI SWEETY",
+        "id": "12",
+        "name": "TERE VAASTAE",
+        "movie": "ZARA HATKE ZARA BACHKE"
     },
     {
-        id: "13",
-        name: "TUM HEE HO BANDHU",
-        movie: "COCKTAIL",
+        "id": "13",
+        "name": "TUM HEE HO BANDHU",
+        "movie": "COCKTAIL"
     },
     {
-        id: "14",
-        name: "KASHMIR MAI TU KANYA KUMARI ",
-        movie: "CHENNAI EXPRESS",
+        "id": "14",
+        "name": "SUIT SUIT",
+        "movie": "HINDI MEDIUM"
     },
     {
-        id: "15",
-        name: "DILDARA ",
-        movie: "RA.ONE",
+        "id": "15",
+        "name": "PYAAR HOTA KAI BAAR HAI",
+        "movie": "KYUKI TUMHE PATA HAI"
     },
     {
-        id: "16",
-        name: "NASHE SE CHAD GAYI",
-        movie: "BEFIKRE",
+        "id": "16",
+        "name": "TU MERI",
+        "movie": "BANG BANG"
     },
     {
-        id: "17",
-        name: "TWIST",
-        movie: "LOVE AAJ KAL",
+        "id": "17",
+        "name": "TWIST",
+        "movie": "LOVE AAJ KAL"
     },
     {
-        id: "18",
-        name: "MANN MA EMOTION JAAGE ",
-        movie: "DILWALE",
+        "id": "18",
+        "name": "MANN MA EMOTION JAAGE",
+        "movie": "DILWALE"
     },
     {
-        id: "19",
-        name: "MAI TERA BOYFRIEND",
-        movie: "RAABTA",
+        "id": "19",
+        "name": "MAI TERA BOYFRIEND",
+        "movie": "RAABTA"
     },
     {
-        id: "20",
-        name: "JAANE KYU",
-        movie: "DOSTANA ",
+        "id": "20",
+        "name": "JAANE KYU",
+        "movie": "DOSTANA"
     },
     {
-        id: "21",
-        name: "BESHARMI KI HEIGHT ",
-        movie: "MAI TERA HERO",
+        "id": "21",
+        "name": "BESHARMI KI HEIGHT",
+        "movie": "MAI TERA HERO"
     },
     {
-        id: "22",
-        name: "MASAKALLI",
-        movie: "DELHI 6",
+        "id": "22",
+        "name": "ILLEGAL WEAPON 2.O",
+        "movie": "STREET DANCER 3D"
     },
     {
-        id: "23",
-        name: "BOL NA HALKE ",
-        movie: "JHOOM BARABAR JHOOM",
+        "id": "23",
+        "name": "BOL NA HALKE",
+        "movie": "JHOOM BARABAR JHOOM"
     },
     {
-        id: "24",
-        name: "PREM KI NAIYA",
-        movie: "AJAB PREM KI GAJAB KAHANI",
+        "id": "24",
+        "name": "PREM KI NAIYA",
+        "movie": "AJAB PREM KI GAJAB KAHANI"
     },
     {
-        id: "25",
-        name: "NADAAN PARINDE ",
-        movie: "ROCKSTAR",
+        "id": "25",
+        "name": "CHARACTER DHEELA",
+        "movie": "READY"
     },
     {
-        id: "26",
-        name: "CHASHNI ",
-        movie: "BHARAT",
+        "id": "26",
+        "name": "CHASHNI",
+        "movie": "BHARAT"
     },
     {
-        id: "27",
-        name: "LAUNG DA LASKARA ",
-        movie: "PATIALA HOUSE",
+        "id": "27",
+        "name": "ATAK GAYA",
+        "movie": "BADHAI DO"
     },
     {
-        id: "28",
-        name: "TERE BIN ",
-        movie: "SIMMBA",
+        "id": "28",
+        "name": "TERE BIN",
+        "movie": "SIMMBA"
     },
     {
-        id: "29",
-        name: "JEENE LAGA HU",
-        movie: "RAMAIYA VASTAMAIYA",
+        "id": "29",
+        "name": "LOOT FUTT GAYA",
+        "movie": "DUNKI"
     },
     {
-        id: "30",
-        name: "ISHQ WALA LOVE ",
-        movie: "STUDENT OF THE YEAR",
+        "id": "30",
+        "name": "MAIN DHOONDHNE KO ZAMANE MAI",
+        "movie": "HEARTLESS"
     },
     {
-        id: "31",
-        name: "O MAAHI O MAAHI ",
-        movie: "DUNKI",
+        "id": "31",
+        "name": "MAIN AGAR",
+        "movie": "TUBE LIGHT"
     },
     {
-        id: "32",
-        name: "JAB SE TERE NAINA ",
-        movie: "SAWARIYA",
+        "id": "32",
+        "name": "JAB SE TERE NAINA",
+        "movie": "SAWARIYA"
     },
     {
-        id: "33",
-        name: "NADIYO PAAR ",
-        movie: "ROOHI",
+        "id": "33",
+        "name": "NADIYO PAAR",
+        "movie": "ROOHI"
     },
     {
-        id: "34",
-        name: "WHAT JHOOMKA",
-        movie: "ROCKY AUR RANI KI PREM KAHANI",
+        "id": "34",
+        "name": "WHAT JHOOMKA",
+        "movie": "ROCKY AUR RANI KI PREM KAHANI"
     },
     {
-        id: "35",
-        name: "JEHDA NASHA",
-        movie: "AN ACTION HERO",
+        "id": "35",
+        "name": "ISHQ KI BAAJIYAAN",
+        "movie": "SOORMA"
     },
     {
-        id: "36",
-        name: "CHIKNI CHAMELI",
-        movie: "AGNEEPATH",
+        "id": "36",
+        "name": "IKTARA",
+        "movie": "WAKE UP SID"
     },
     {
-        id: "37",
-        name: "PSYCHO SAIYAAN",
-        movie: "SAHOO",
+        "id": "37",
+        "name": "PSYCHO SAIYAAN",
+        "movie": "SAHOO"
     },
     {
-        id: "38",
-        name: "COCA COLA",
-        movie: "LUKA CHUPPI",
+        "id": "38",
+        "name": "MERE BINA",
+        "movie": "CROOK"
     },
     {
-        id: "39",
-        name: "CHOGADA",
-        movie: "LOVE YATRI",
+        "id": "39",
+        "name": "THUMKESHWARI",
+        "movie": "BHEDIYA"
     },
     {
-        id: "40",
-        name: "HOOKAAH BAAR ",
-        movie: "KHILADI 786",
+        "id": "40",
+        "name": "DHATING NAACH",
+        "movie": "PHATA POSTER NIKLA HERO"
     },
     {
-        id: "41",
-        name: "TUMHE JO MAINE DEKHA",
-        movie: "MAIN HOON NA",
+        "id": "41",
+        "name": "SAU TARAH KE",
+        "movie": "DHISHOOM"
     },
     {
-        id: "42",
-        name: "CHITIYA KALAIYAN",
-        movie: "ROY",
+        "id": "42",
+        "name": "NAACHENGE SAARI RAAT",
+        "movie": "JUNOONIYAT"
     },
     {
-        id: "43",
-        name: "ISHQ BULAVA ",
-        movie: "HASEE TOH PHASEE",
+        "id": "43",
+        "name": "PARAM SUNDARI",
+        "movie": "MIMI"
     },
     {
-        id: "44",
-        name: "AGAR TUM SAATH HO",
-        movie: "TAMASHA",
+        "id": "44",
+        "name": "AGAR TUM SAATH HO",
+        "movie": "TAMASHA"
     },
     {
-        id: "45",
-        name: "LAT LAG GAYI",
-        movie: "RACE 2",
+        "id": "45",
+        "name": "HAULI HAULI",
+        "movie": "DE DE PYAAR DE"
     },
     {
-        id: "46",
-        name: " SAJDA",
-        movie: "MY NAME IS KHAN ",
+        "id": "46",
+        "name": "SAJDA",
+        "movie": "MY NAME IS KHAN"
     },
     {
-        id: "47",
-        name: "NAJAA",
-        movie: "SOORYAVANSHI",
+        "id": "47",
+        "name": "CHALTI HAI KYA 9 SE 12",
+        "movie": "JUDWA 2"
     },
     {
-        id: "48",
-        name: "YAAR NA MILE",
-        movie: "KICK",
+        "id": "48",
+        "name": "YAAR NA MILE",
+        "movie": "KICK"
     },
     {
-        id: "49",
-        name: "TUM TAK",
-        movie: "RANJHANA",
+        "id": "49",
+        "name": "TUM TAK",
+        "movie": "RANJHANA"
     },
     {
-        id: "50",
-        name: "O BEDARDEYA",
-        movie: "TU JHUTHI MAI MAKAAR",
+        "id": "50",
+        "name": "SOORAJ DOOBA HAI",
+        "movie": "ROY"
     },
     {
-        id: "51",
-        name: "CHAND SIFARISH",
-        movie: "FANAA",
+        "id": "51",
+        "name": "HUSN HAI SUHANA",
+        "movie": "COOLIE NO.1(NEW)"
     },
     {
-        id: "52",
-        name: "JEENA JEENA ",
-        movie: "BADLAPUR",
+        "id": "52",
+        "name": "JEENA JEENA",
+        "movie": "BADLAPUR"
     },
     {
-        id: "53",
-        name: "KHUDA JAANE ",
-        movie: "BACHNA E HASEENO",
+        "id": "53",
+        "name": "KHUDA JAANE",
+        "movie": "BACHNA E HASEENO"
     },
     {
-        id: "54",
-        name: "TERI ORE",
-        movie: "SINGH IS KING",
+        "id": "54",
+        "name": "TERI ORE",
+        "movie": "SINGH IS KING"
     },
     {
-        id: "55",
-        name: "TE AMO",
-        movie: "DUM MARO DUM",
+        "id": "55",
+        "name": "TE AMO",
+        "movie": "DUM MARO DUM"
     },
     {
-        id: "56",
-        name: "MOH MOH KE DHAAGE",
-        movie: "DUM LAGA KE HAISA",
+        "id": "56",
+        "name": "SUBAH HONE NA DE",
+        "movie": "DESI BOYS"
     },
     {
-        id: "57",
-        name: "O RANGREZ",
-        movie: "BHAAG MILKA BHAAG",
+        "id": "57",
+        "name": "O RANGREZ",
+        "movie": "BHAAG MILKA BHAAG"
     },
     {
-        id: "58",
-        name: "SAWARE",
-        movie: "PHANTOM",
+        "id": "58",
+        "name": "SAWARE",
+        "movie": "PHANTOM"
     },
     {
-        id: "59",
-        name: "LADKI BADI ANJANI HAI",
-        movie: "KUCH KUCH TOH HOTA HAI",
+        "id": "59",
+        "name": "LADKI BADI ANJANI HAI",
+        "movie": "KUCH KUCH TOH HOTA HAI"
     },
     {
-        id: "60",
-        name: "BHOLI SI SURAT",
-        movie: "DIL TOH PAGAL HAI",
+        "id": "60",
+        "name": "BHOLI SI SURAT",
+        "movie": "DIL TOH PAGAL HAI"
     },
     {
-        id: "61",
-        name: "DIL KYU YEH MERA",
-        movie: "KITES",
+        "id": "61",
+        "name": "DIL KYUN YEH MERA",
+        "movie": "KITES"
     },
     {
-        id: "62",
-        name: "KABHI KABHI ADITI",
-        movie: "JAANE TU YA JAANE NA",
+        "id": "62",
+        "name": "TUM MILE",
+        "movie": "TUM MILE"
     },
     {
-        id: "63",
-        name: "MAIN AGAR KAHOON",
-        movie: "OM SHANTI OM",
+        "id": "63",
+        "name": "MARJAWA",
+        "movie": "FASHION"
     },
     {
-        id: "64",
-        name: "ISHQ SUFIYANA",
-        movie: "THE DIRTY PICTURE",
+        "id": "64",
+        "name": "YEH JAWAANI HAI DEEWANI",
+        "movie": "YJHD"
     },
     {
-        id: "65",
-        name: "MUQABLA",
-        movie: "STREET DANCER",
+        "id": "65",
+        "name": "ROOBAROO",
+        "movie": "RANG DE BASANTI"
     },
     {
-        id: "66",
-        name: "HAR FUN MAULA",
-        movie: "KOI JAANE NA",
+        "id": "66",
+        "name": "KABIRA",
+        "movie": "YEH JAWAANI HAI DEEWANI"
     },
     {
-        id: "67",
-        name: "O JAANE JAANA",
-        movie: "PYAAR KIYA TOH DARNA KYA",
+        "id": "67",
+        "name": "GALLAN GOODIYAN",
+        "movie": "DIL DHADAKNE DO"
     },
     {
-        id: "68",
-        name: "SAWAAR LOON",
-        movie: "LOOTERA",
+        "id": "68",
+        "name": "RABTA",
+        "movie": "AGENT VINOD"
     },
     {
-        id: "69",
-        name: "BHAAG DK BOSE",
-        movie: "DELHI BELLY",
+        "id": "69",
+        "name": "BANDOOK MERE YAAR",
+        "movie": "YAARA"
     },
     {
-        id: "70",
-        name: "NAZAR NA LAG JAAYE ",
-        movie: "STREE ",
+        "id": "70",
+        "name": "JALEBI",
+        "movie": "JISM 2"
     },
     {
-        id: "71",
-        name: "JAG GHOOMEYA ",
-        movie: "SULTAN",
+        "id": "71",
+        "name": "BAN JA RANI",
+        "movie": "TUMHARI SULU"
     },
     {
-        id: "72",
-        name: "DEKHTE DEKHTE",
-        movie: "BATTI GUL METER CHALU",
+        "id": "72",
+        "name": "SAMJHAWAN",
+        "movie": "HUMPTY SHARMA KI DULHANIA"
     },
     {
-        id: "73",
-        name: "ZAALIMA",
-        movie: "RAEES",
+        "id": "73",
+        "name": "GAAL NI KADNI",
+        "movie": "SONG"
     },
     {
-        id: "74",
-        name: "TERA FITOOR",
-        movie: "GENIUS",
+        "id": "74",
+        "name": "MERE YAARA",
+        "movie": "SOORYAVANSHI"
     },
     {
-        id: "75",
-        name: "TERA CHEHRA",
-        movie: "SANAM TERI KASAM",
+        "id": "75",
+        "name": "MANN BHARYA",
+        "movie": "SHERSHAAH"
     },
     {
-        id: "76",
-        name: "PANI DA RANG",
-        movie: "VICKY DONOR",
+        "id": "76",
+        "name": "RATAN LAMBIYAN",
+        "movie": "SHERSHAAH"
     },
     {
-        id: "77",
-        name: "AINVAYI AINVAYI",
-        movie: "BAND BAJA BAARAT",
+        "id": "77",
+        "name": "OSHO HONA CHAANDIYA",
+        "movie": "SHIDDAT"
     },
     {
-        id: "78",
-        name: "FIRST CLASS",
-        movie: "KALANK",
+        "id": "78",
+        "name": "VEHAM",
+        "movie": "BAJRE DA SITTA"
     },
     {
-        id: "79",
-        name: "JHAK MAAR KE",
-        movie: "DESI BOYZ",
+        "id": "79",
+        "name": "YEH DIL DEEWANA",
+        "movie": "PARDES"
     },
     {
-        id: "80",
-        name: "DEEWANA HAI DEKHO",
-        movie: "KABHI KHUSHI KABHI GUM",
+        "id": "80",
+        "name": "KAUN TUJHE",
+        "movie": "MS DHONI"
     },
     {
-        id: "81",
-        name: "SURILI ANKHIYO WALE",
-        movie: "VEER",
+        "id": "81",
+        "name": "TERA HONE LAGA HOON",
+        "movie": "AJAB PREM KI GAJAB KAHANI"
     },
     {
-        id: "82",
-        name: "MATARGHASTI",
-        movie: "TAMASHA",
+        "id": "82",
+        "name": "NAACHENGE SAARI RAAT",
+        "movie": "BAAR BAAR DEKHO"
     },
     {
-        id: "83",
-        name: "MERA NAAM TU",
-        movie: "ZERO",
+        "id": "83",
+        "name": "KALA CHASHMA",
+        "movie": "BAAR BAAR DEKHO"
     },
     {
-        id: "84",
-        name: "BAAZIGAR O BAAZIGAR",
-        movie: "BAAZIGAR",
+        "id": "84",
+        "name": "LAUNG DA LASHKARA",
+        "movie": "PATIALA HOUSE"
     },
     {
-        id: "85",
-        name: "SAAT SAMUNDAR",
-        movie: "VISWATMA",
+        "id": "85",
+        "name": "CUTIE PIE",
+        "movie": "AE DIL HAI MUSHKIL"
     },
     {
-        id: "86",
-        name: "TERE MAST MAST DO NAIN",
-        movie: "DABANGG",
+        "id": "86",
+        "name": "ACHO SAIYAAN",
+        "movie": "BADLAPUR"
     },
     {
-        id: "87",
-        name: "MAIN RANG SHARBATON",
-        movie: "PHATA POSTER NIKLA HERO",
+        "id": "87",
+        "name": "TUM JO AAYE",
+        "movie": "ONCE UPON A TIME IN MUMBAI"
     },
     {
-        id: "88",
-        name: "PEHLI NAZAR MAIN",
-        movie: "RACE ",
+        "id": "88",
+        "name": "BAKHEDAA",
+        "movie": "TOILET: EK PREM KATHA"
     },
     {
-        id: "89",
-        name: "SADI GALI",
-        movie: "TANU WEDS MANU",
+        "id": "89",
+        "name": "BULLEYA",
+        "movie": "AE DIL HAI MUSHKIL"
     },
     {
-        id: "90",
-        name: "OH HO HO ",
-        movie: "HINDI MEDIUM",
+        "id": "90",
+        "name": "SANAM RE",
+        "movie": "SANAM RE"
     },
     {
-        id: "91",
-        name: "DIL MAI BAJI GUITAR",
-        movie: "APNA SAPNA MONEY MONEY",
+        "id": "91",
+        "name": "NAGIN",
+        "movie": "BAZAR"
     },
     {
-        id: "92",
-        name: "ZARA ZARA",
-        movie: "REHNA HAI TERE DIL MAI",
+        "id": "92",
+        "name": "ANKH LAD JAAVE",
+        "movie": "LOVEYATRI"
     },
     {
-        id: "93",
-        name: "DIL NA JAANEYA",
-        movie: "GOOD NEWS",
+        "id": "93",
+        "name": "NACHDE NE SAARE",
+        "movie": "BAAR BAAR DEKHO"
     },
     {
-        id: "94",
-        name: "BOL DO NA ZARA",
-        movie: "AZHAR",
+        "id": "94",
+        "name": "VE MAHI",
+        "movie": "KESARI"
     },
     {
-        id: "95",
-        name: "EK DIL EK JAAN ",
-        movie: "PADMAVAT",
+        "id": "95",
+        "name": "CHAL GALIYYAN",
+        "movie": "KABIR SINGH"
     },
     {
-        id: "96",
-        name: "PIYA O RE PIYA",
-        movie: "TERE NAAL LOVE HO GAYA",
+        "id": "96",
+        "name": "JALIMA",
+        "movie": "RAEES"
     },
     {
-        id: "97",
-        name: "MANN MERA ",
-        movie: "TABLE NO.21",
+        "id": "97",
+        "name": "SOCH NA SAKE",
+        "movie": "AIRLIFT"
     },
     {
-        id: "98",
-        name: "CHAMMAK CHALLO",
-        movie: "RA.ONE",
+        "id": "98",
+        "name": "GERUA",
+        "movie": "DILWALE"
     },
     {
-        id: "99",
-        name: "YEH FITOOR MERA ",
-        movie: "FITOOR",
+        "id": "99",
+        "name": "HAWA HAWA",
+        "movie": "MUBARAKAN"
     },
     {
-        id: "100",
-        name: "SHIRT DA BUTTON",
-        movie: "KYA SUPER COOL HAI HUM",
-    },
-];
+        "id": "100",
+        "name": "KAUN HOON MAIN",
+        "movie": "AQUAMAN"
+    }
+]
+
 
 songData.forEach((song) => {
     song.name = song.name.toLocaleLowerCase();
